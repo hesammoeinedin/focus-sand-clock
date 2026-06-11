16a9fb6a25e4b0655c73b6e98dfaf73028f25223
Hesam Moeinedin <hesam.moeinedin@gmail.com>
2026-06-11 15:24:16 +0200

    Improve widget: transparent bg, smooth blue sand, resizable window, mini mode

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
 
 let win
+let isMini = false
+const NORMAL_W = 260, NORMAL_H = 540
+const MINI_W = 64, MINI_H = 64
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 260,
-    height: 520,
-    minWidth: 180,
-    minHeight: 360,
+    width: NORMAL_W,
+    height: NORMAL_H,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
     resizable: true,
+    minWidth: 160,
+    minHeight: 320,
     skipTaskbar: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
-
   win.loadFile('index.html')
 }
 
 app.whenReady().then(createWindow)
+app.on('window-all-closed', () => app.quit())
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
+ipcMain.on('close-app', () => app.quit())
 
-ipcMain.on('close-app', () => {
-  app.quit()
-})
-
-ipcMain.on('minimize-app', () => {
-  win.minimize()
+ipcMain.on('toggle-mini', () => {
+  if (!isMini) {
+    win._normalSize = win.getSize()
+    win.setResizable(false)
+    win.setMinimumSize(MINI_W, MINI_H)
+    win.setSize(MINI_W, MINI_H)
+    isMini = true
+  } else {
+    const [w, h] = win._normalSize || [NORMAL_W, NORMAL_H]
+    win.setMinimumSize(160, 320)
+    win.setSize(w, h)
+    win.setResizable(true)
+    isMini = false
+  }
+  win.webContents.send('mini-state', isMini)
 })
